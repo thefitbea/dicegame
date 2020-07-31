a5a177c58bd4a988f9200c516a94b059bedc361b
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2020-07-31 17:59:00 +0530

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
-document
+var numberRandom1 = Math.floor((Math.random()*6)+1);
+var diceImageRandom = "images/dice" + numberRandom1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+
+if(numberRandom1 === 1){
+  image1.setAttribute("src","images/dice1.png");
+}
+else if (numberRandom1 === 2) {
+  image1.setAttribute("src","images/dice2.png");
+}
+else if (numberRandom1 === 3) {
+  image1.setAttribute("src","images/dice3.png");
+}
+else if (numberRandom1 === 4) {
+  image1.setAttribute("src","images/dice4.png");
+}
+else if (numberRandom1 === 5) {
+  image1.setAttribute("src","images/dice5.png");
+}
+else {
+  image1.setAttribute("src","images/dice6.png");
+}
